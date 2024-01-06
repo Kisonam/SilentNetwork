44319716301502283fbefe2eccd2ff05eb0f417b
Kisonam <kisonamplay@gmail.com>
2024-01-06 23:22:41 +0100

    create hub connection for manage users

---
+++ SilentNetwork/SilentNetwork/Hubs/HubConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace SilentNetwork.Hubs
+{
+	public static class HubConnection
+	{
+		public static Dictionary<string, List<string>> Users = new();
+		// <Lobby name, Connections in lobby> 
+        public static Dictionary<string, List<string>> Lobbys = new();
+
+
+		public static List<string> OnlineUsers()
+		{
+			return Users.Keys.ToList();
+		}
+    }
+}
+
