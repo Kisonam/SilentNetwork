eed6b54715415387052ed0330520d9691c33de34
Kisonam <kisonamplay@gmail.com>
2024-01-07 17:11:34 +0100

    End HubConnection part for manage user connection

--- SilentNetwork/SilentNetwork/Hubs/HubConnection.cs
+++ SilentNetwork/SilentNetwork/Hubs/HubConnection.cs
@@ -7,8 +7,53 @@ namespace SilentNetwork.Hubs
 		// <Lobby name, Connections in lobby> 
         public static Dictionary<string, List<string>> Lobbys = new();
 
+        public static bool HasUserConnection(string UserId, string ConnectionId)
+        {
+            try
+            {
+                if (Users.ContainsKey(UserId))
+                {
+                    return Users[UserId].Any(p => p.Contains(ConnectionId));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-		public static List<string> OnlineUsers()
+            return false;
+        }
+
+        public static bool HasConnection(string UserId)
+        {
+            try
+            {
+                if (Users.ContainsKey(UserId))
+                {
+                    return Users[UserId].Any();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return false;
+        }
+
+        public static void AddUserConnection(string UserId, string ConnectionId)
+        {
+
+            if (!string.IsNullOrEmpty(UserId) && !HasUserConnection(UserId, ConnectionId))
+            {
+                if (Users.ContainsKey(UserId))
+                    Users[UserId].Add(ConnectionId);
+                else
+                    Users.Add(UserId, new List<string> { ConnectionId });
+            }
+        }
+
+        public static List<string> OnlineUsers()
 		{
 			return Users.Keys.ToList();
 		}
